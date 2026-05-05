3ab7efa3b9ac9627d79433e1a043819179c4db91
smgaf <smgaf@DESKTOP-7K16M50>
2026-05-05 15:01:46 +0300

    Add project files.

---
+++ The Smart Number Guesser/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Smart_Number_Guesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int targetNumber = rnd.Next(1, 101);
+
+            int guessnumber = 0;
+            int count = 0;
+
+          
+            while (guessnumber != targetNumber)
+            {
+                Console.Write("Enter your guess: ");
+                guessnumber = int.Parse(Console.ReadLine()); 
+                count++;
+
+                if (guessnumber < targetNumber)
+                {
+                    Console.WriteLine("Go HIGHER! ⬆️");
+                }
+                else if (guessnumber > targetNumber)
+                {
+                    Console.WriteLine("Go LOWER! ⬇️");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! 🎉");
+                    Console.WriteLine("You found it in " + count + " attempts.");
+                }
+
+
+
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
